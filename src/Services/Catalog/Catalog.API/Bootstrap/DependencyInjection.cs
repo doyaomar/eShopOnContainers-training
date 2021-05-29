ddc85a516357f7834f13e7c2422381adfcfe0125
Omar Doya <doya.omar@outlook.com>
2021-05-29 01:03:33 +0100

    add self check

--- src/Services/Catalog/Catalog.API/Bootstrap/DependencyInjection.cs
+++ src/Services/Catalog/Catalog.API/Bootstrap/DependencyInjection.cs
@@ -3,6 +3,7 @@ using Catalog.API.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Catalog.API.Bootsrap
 {
@@ -17,7 +18,7 @@ namespace Catalog.API.Bootsrap
 
             return services;
         }
-        
+
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services
@@ -29,6 +30,7 @@ namespace Catalog.API.Bootsrap
         public static IServiceCollection AddCustomHealhChecks(this IServiceCollection services)
         {
             services.AddHealthChecks()
+            .AddCheck("self", () => HealthCheckResult.Healthy())
             .AddDbContextCheck<CatalogContext>();
 
             return services;
