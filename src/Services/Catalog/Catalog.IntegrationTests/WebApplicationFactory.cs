164bd42b61633c9e0f205affdcbdc026b8d5d3db
Omar Doya <doya.omar@gmail.com>
2021-12-04 00:57:53 +0100

    fix integration tests

--- src/Services/Catalog/Catalog.IntegrationTests/WebApplicationFactory.cs
+++ src/Services/Catalog/Catalog.IntegrationTests/WebApplicationFactory.cs
@@ -2,15 +2,24 @@ namespace Catalog.IntegrationTests;
 
 public class WebApplicationFactory : WebApplicationFactory<Program>
 {
+
+    private MongoDbRunner _runner = default!;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        var runner = MongoDbRunner.Start();
-        MongoClient client = new(runner.ConnectionString);
+        _runner = MongoDbRunner.Start();
+        _runner.Import("CatalogDb", "catalogItems", @"Data\catalogItems.json", false);
 
         builder
         .ConfigureTestServices(services =>
         {
-            services.AddSingleton<IMongoClient>(_ => client);
+            services.AddSingleton<IMongoClient>(_ => new MongoClient(_runner.ConnectionString));
         });
+    }
+
+    public override ValueTask DisposeAsync()
+    {
+        _runner.Dispose();
+        return base.DisposeAsync();
     }
 }
