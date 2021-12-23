f44b425f73b2de2450f52c1be1955029123eae47
Omar Doya <doya.omar@gmail.com>
2021-12-23 21:31:45 +0100

    refacto

--- src/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
@@ -39,21 +39,35 @@ public class CatalogDbContext : ICatalogDbContext
     public async Task<CatalogItem?> FindAsync(Guid id, CancellationToken cancellationToken = default)
     => await CatalogItems.Find(x => x.Id == id).SingleOrDefaultAsync(cancellationToken);
 
-    public async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindAllAsync(IEnumerable<Guid> ids, int page, int size, CancellationToken cancellationToken = default)
+    public async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindAllAsync(
+        IEnumerable<Guid> ids,
+        int page,
+        int size,
+        CancellationToken cancellationToken = default)
     {
         Expression<Func<CatalogItem, bool>> filter = ids.Any() ? x => ids.Contains(x.Id) : _ => true;
 
         return await FindCatalogItemsAsync(page, size, filter, cancellationToken);
     }
 
-    public async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindByTypeAndBrandAsync(Guid typeId, Guid? brandId, int page, int size, CancellationToken cancellationToken = default)
+    public async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindByTypeAndBrandAsync(
+        Guid typeId,
+        Guid? brandId,
+        int page,
+        int size,
+        CancellationToken cancellationToken = default)
     {
-        Expression<Func<CatalogItem, bool>> filter = brandId is null ? x => x.CatalogType.Id == typeId : x => x.CatalogType.Id == typeId && x.CatalogBrand.Id == brandId;
+        Expression<Func<CatalogItem, bool>> filter = brandId is null
+        ? x => x.CatalogType.Id == typeId
+        : x => x.CatalogType.Id == typeId && x.CatalogBrand.Id == brandId;
 
         return await FindCatalogItemsAsync(page, size, filter, cancellationToken);
     }
 
-    private async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindCatalogItemsAsync(int page, int size, Expression<Func<CatalogItem, bool>> filter, CancellationToken cancellationToken)
+    private async Task<(IReadOnlyCollection<CatalogItem> Items, long Count)> FindCatalogItemsAsync(int page,
+        int size,
+        Expression<Func<CatalogItem, bool>> filter,
+        CancellationToken cancellationToken)
     {
         var items = CatalogItems.Find(filter)
                                 .SortByDescending(x => x.Name)
