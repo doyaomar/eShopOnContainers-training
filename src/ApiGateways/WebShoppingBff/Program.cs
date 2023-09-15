13e00be4747370e5d216c142435c8459cc4cf63c
Omar Doya <doya.omar@outlook.com>
2023-09-15 17:43:52 +0200

    init webshopping bff

---
+++ src/ApiGateways/WebShoppingBff/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration
+.SetBasePath(builder.Environment.ContentRootPath)
+.AddJsonFile("appsettings.json", true, true)
+.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+//.AddJsonFile("ocelot.json", false, true)
+.AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
+
+app.Run();
